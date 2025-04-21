e0d18315991fe790ca54b90f3491386bd34ef740
helloca <815612516@qq.com>
2025-04-21 15:23:44 +0800

    ceshi

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+const mike = {
+  name: 'Mike',
+  age: 18
+};
+
+
+var a = {
+
+
+
+
+
+
+
+
+}
